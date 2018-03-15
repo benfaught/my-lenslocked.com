6946e558929c1dfaa1aa88fa7a1b0376885dac10
Benjamin Faught <benjaminfaught007@gmail.com>
2018-03-15 18:52:36 -0500

    Added a content-type header

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":3000", nil)
 }
