d268d9a532d5df55207b0257766723bf14a4a069
Benjamin Faught <benjaminfaught007@gmail.com>
2018-03-27 15:17:17 -0500

    Added in the schema package and used it to parse our forms into the SignupForm type that we just created

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
+
 	"lenslocked.com/views"
 )
 
@@ -17,16 +19,21 @@ type Users struct {
 	NewView *views.View
 }
 
+type SignupForm struct {
+	//using struct tags because the name on form and struct arent matching
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 //New is used to render the form where a user can create
 //a new user account.
 //
 //GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("is this working?")
 	u.NewView.Render(w, nil)
 }
 
-//This is used to process the signup form when a user tries to
+//Create is used to process the signup form when a user tries to
 //create a new user account.
 //
 //POST /signup
@@ -34,6 +41,11 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostForm["password"])
+
+	dec := schema.NewDecoder()
+	form := SignupForm{}
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
