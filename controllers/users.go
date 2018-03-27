a978a728ab1642c93bf01aec414f202c859373a7
Benjamin Faught <benjaminfaught007@gmail.com>
2018-03-26 19:16:54 -0500

    Created our 1st controller

---
+++ controllers/users.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"lenslocked.com/views"
+)
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+//New is used to render the form where a user can create
+//a new user account.
+//
+//GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("is this working?")
+	u.NewView.Render(w, nil)
+}
+
+//This is used to process the signup form when a user tries to
+//create a new user account.
+//
+//POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a fake message. Pretend that we created a user account")
+}
--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"lenslocked.com/controllers"
 	"lenslocked.com/views"
 )
 
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -23,20 +23,17 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
-
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
+	r.HandleFunc("/signup", usersC.Create).Methods("POST")
+
 	http.ListenAndServe(":3000", r)
 }
 
