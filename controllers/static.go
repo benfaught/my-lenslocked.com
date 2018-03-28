340f349f015c494d1a162c8a8089661899467c8b
Benjamin Faught <benjaminfaught007@gmail.com>
2018-03-28 14:36:12 -0500

    Made it easier to create views by not passing in the views/ prefix and by not requiring the gohtml extension

--- controllers/static.go
+++ controllers/static.go
@@ -6,8 +6,8 @@ import (
 
 func NewStatic() *Static {
 	return &Static{
-		Home:    views.NewView("bootstrap", "views/static/home.gohtml"),
-		Contact: views.NewView("bootstrap", "views/static/contact.gohtml"),
+		Home:    views.NewView("bootstrap", "static/home"),
+		Contact: views.NewView("bootstrap", "static/contact"),
 	}
 }
 
--- views/view.go
+++ views/view.go
@@ -8,10 +8,13 @@ import (
 
 var (
 	LayoutDir   string = "views/layouts/"
+	TemplateDir string = "views/"
 	TemplateExt string = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
+	addTemplatePath(files)
+	addTemplateExt(files)
 	files = append(files, layoutfiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -35,7 +38,7 @@ func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Render is used to render the view with the predefined layout.
+//Render is used to render the view with the predefined layout.
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 	w.Header().Set("Content-Type", "text/html")
 	return v.Template.ExecuteTemplate(w, v.Layout, data)
@@ -49,4 +52,28 @@ func layoutfiles() []string {
 		panic(err)
 	}
 	return files
+}
+
+// addTemplatePath takes in a slice of strings
+// representing file paths for templates, and it prepends
+// the TemplateDir directory to each string in the slice
+//
+// Eg the input {"Home"} would result in the output
+// {"views/home"} if TemplateDir == "views/"
+func addTemplatePath(files []string) {
+	for i, f := range files {
+		files[i] = TemplateDir + f
+	}
+}
+
+// addTemplatePath takes in a slice of strings
+// representing file paths for templates and it appends
+// the TemplateExt extension to each string in the slice
+//
+// Eg the input {"home"} would result in the output
+// {"home.gohtml"} if TemplateExt == ".gohtml"
+func addTemplateExt(files []string) {
+	for i, f := range files {
+		files[i] = f + TemplateExt
+	}
 }
