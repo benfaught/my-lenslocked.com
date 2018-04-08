8f60cf39d53efc9447d54ed06198b610796fa0f4
Benjamin Faught <benjaminfaught007@gmail.com>
2018-04-08 16:51:10 -0500

    Created the User model

---
+++ models/users.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
