fcbc2840b52f50b1e0098ec76f51bbb9c39e9a7f
Benjamin Faught <benjaminfaught007@gmail.com>
2018-04-09 09:12:38 -0500

    implemented the Create method in our UserService: models/user.go

--- models/users.go
+++ models/users.go
@@ -50,6 +50,12 @@ func (us *UserService) ByID(id uint) (*User, error) {
 	}
 }
 
+// Create will create the provided user and backfill data
+// like the ID, CreatedAt, and UpdatedAt fields.
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
 // Closes the UserService database connection
 func (us *UserService) Close() error {
 	return us.db.Close()
