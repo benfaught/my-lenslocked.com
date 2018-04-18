74ef17acddcf1db6f944e5727096101277b74af4
Benjamin Faught <benjaminfaught007@gmail.com>
2018-04-17 21:07:38 -0500

    Added a controller action to view cookies

--- controllers/users.go
+++ controllers/users.go
@@ -94,3 +94,13 @@ func (u *Users) Login(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintln(w, user)
 }
+
+// CookieTest is used to display cookies on the current user
+func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("email")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(w, "Email is:", cookie.Value)
+}
--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func main() {
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	r.Handle("/login", usersC.LoginView).Methods("GET")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
+	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
 	fmt.Println("starting the server at :3000")
 	http.ListenAndServe(":3000", r)
 }
