c7a5ae1fa9ea37d6290846712a70c08657614721
Benjamin Faught <benjaminfaught007@gmail.com>
2018-05-15 17:27:48 -0500

    Disabled using our sign in cookies with JS

--- controllers/users.go
+++ controllers/users.go
@@ -115,8 +115,9 @@ func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
 	}
 
 	cookie := http.Cookie{
-		Name:  "remember_token",
-		Value: user.Remember,
+		Name:     "remember_token",
+		Value:    user.Remember,
+		HttpOnly: true,
 	}
 	http.SetCookie(w, &cookie)
 	return nil
