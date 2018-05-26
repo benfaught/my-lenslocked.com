e4245d48e6f8964c5dc80937266ee432dd1cddb5
Benjamin Faught <benjaminfaught007@gmail.com>
2018-05-26 11:45:39 -0500

    Implemented the idGreaterThan(n) user validator creator

--- models/users.go
+++ models/users.go
@@ -188,8 +188,11 @@ func (uv *userValidator) Update(user *User) error {
 
 // Delete will delete the user with the provided ID
 func (uv *userValidator) Delete(id uint) error {
-	if id == 0 {
-		return ErrInvalidID
+	var user User
+	user.ID = id
+	err := runUserValFuncs(&user, uv.idGreaterThan(0))
+	if err != nil {
+		return err
 	}
 	return uv.UserDB.Delete(id)
 }
@@ -229,6 +232,15 @@ func (uv *userValidator) setRememberIfUnset(user *User) error {
 	}
 	user.Remember = token
 	return nil
+}
+
+func (uv *userValidator) idGreaterThan(n uint) userValFunc {
+	return userValFunc(func(user *User) error {
+		if user.ID <= n {
+			return ErrInvalidID
+		}
+		return nil
+	})
 }
 
 var _ UserDB = &userGorm{}
