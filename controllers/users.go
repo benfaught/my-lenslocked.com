27d0bac5ce39b7c641606779a9730ce0adb604fd
Benjamin Faught <benjaminfaught007@gmail.com>
2018-06-07 11:55:33 -0500

    Implemented error handling for render errors

--- controllers/users.go
+++ controllers/users.go
@@ -36,9 +36,7 @@ type SignupForm struct {
 //
 //GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	if err := u.NewView.Render(w, nil); err != nil {
-		panic(err)
-	}
+	u.NewView.Render(w, nil)
 }
 
 //Create is used to process the signup form when a user tries to
--- views/view.go
+++ views/view.go
@@ -1,7 +1,9 @@
 package views
 
 import (
+	"bytes"
 	"html/template"
+	"io"
 	"net/http"
 	"path/filepath"
 )
@@ -33,13 +35,11 @@ type View struct {
 }
 
 func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if err := v.Render(w, nil); err != nil {
-		panic(err)
-	}
+	v.Render(w, nil)
 }
 
 //Render is used to render the view with the predefined layout.
-func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+func (v *View) Render(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html")
 	switch data.(type) {
 	case Data:
@@ -49,7 +49,12 @@ func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 			Yield: data,
 		}
 	}
-	return v.Template.ExecuteTemplate(w, v.Layout, data)
+	var buf bytes.Buffer
+	if err := v.Template.ExecuteTemplate(&buf, v.Layout, data); err != nil {
+		http.Error(w, "Something went wrong, if the problem persists, please email support@lenslocked.com", http.StatusInternalServerError)
+		return
+	}
+	io.Copy(w, &buf)
 }
 
 // layoutfiles returns a slice of strings representing
