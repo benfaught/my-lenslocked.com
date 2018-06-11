e683d4c48a86944f3b14e64a5a3578b3eb9b9115
Benjamin Faught <benjaminfaught007@gmail.com>
2018-06-11 15:54:35 -0500

    Created the galleries controller and the New action

---
+++ controllers/galleries.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"lenslocked.com/models"
+	"lenslocked.com/views"
+)
+
+func NewGalleries(gs models.GalleryService) *Galleries {
+	return &Galleries{
+		New: views.NewView("bootstrap", "galleries/new"),
+		gs:  gs,
+	}
+}
+
+type Galleries struct {
+	New *views.View
+	gs  models.GalleryService
+}
--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
 
 	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers(services.User)
+	galleriesC := controllers.NewGalleries(services.Gallery)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
@@ -37,6 +38,10 @@ func main() {
 	r.Handle("/login", usersC.LoginView).Methods("GET")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
 	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
+
+	// Gallery routes
+	r.Handle("/galleries/new", galleriesC.New).Methods("GET")
+
 	fmt.Println("starting the server at :3000")
 	http.ListenAndServe(":3000", r)
 }
