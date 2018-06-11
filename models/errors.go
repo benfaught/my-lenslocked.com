30e1b1dc90eb877af25f770a803164b84ca6e9a4
Benjamin Faught <benjaminfaught007@gmail.com>
2018-06-11 18:20:43 -0500

    Implemented the Title and UserID required validators

--- models/errors.go
+++ models/errors.go
@@ -6,42 +6,35 @@ const (
 	// ErrNotFound is returned when a resource cannot be found
 	// in the database.
 	ErrNotFound modelError = "models: resource not found"
-
-	// ErrIDInvalid is returned when an invalid ID is provided
-	// to a method like Delete.
-	ErrIDInvalid modelError = "models: ID provided was invalid"
-
 	// ErrPasswordIncorrect is returned when an invalid password
 	// is used when attempting to authenticate a user.
 	ErrPasswordIncorrect modelError = "models: incorrect password provided"
-
 	// ErrEmailRequired is returned when an email address is
 	// not provided when creating a user
 	ErrEmailRequired modelError = "models: Email address is required"
-
 	// ErrEmailInvalid is returned when an email address
 	// provided does not match any of our requirements
 	ErrEmailInvalid modelError = "models: Email address is not valid"
-
 	// ErrEmailTaken is returned when an update or create is attempted
 	// with an email address that is already in use.
 	ErrEmailTaken modelError = "models: Email address is already taken"
-
 	// ErrPasswordTooShort is returned when an update or create is attempted
 	// with a user password that is less than 8 characters.
 	ErrPasswordTooShort modelError = "models: password must be at least 8 characters"
-
 	// ErrPasswordRequired is returned when a create is attempted
 	// without a user password provided.
 	ErrPasswordRequired modelError = "models: password is required"
-
+	ErrTitleRequired    modelError = "models: title is required"
+	// ErrIDInvalid is returned when an invalid ID is provided
+	// to a method like Delete.
+	ErrIDInvalid privateError = "models: ID provided was invalid"
 	// ErrRememberRequired is returned when a create or update
 	// is attempted without a user remember token hash
-	ErrRememberRequired modelError = "models: remember token is required"
-
+	ErrRememberRequired privateError = "models: remember token is required"
 	// ErrRememberTooShort is returned when a remember token is
 	// not at least 32 bytes
-	ErrRememberTooShort modelError = "models: Remember token must be at least 32 bytes"
+	ErrRememberTooShort privateError = "models: Remember token must be at least 32 bytes"
+	ErrUserIDRequired   privateError = "models: User ID is required"
 )
 
 type modelError string
@@ -55,4 +48,10 @@ func (e modelError) Public() string {
 	split := strings.Split(s, " ")
 	split[0] = strings.Title(split[0])
 	return strings.Join(split, " ")
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
--- models/galleries.go
+++ models/galleries.go
@@ -32,6 +32,30 @@ type galleryValidator struct {
 	GalleryDB
 }
 
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFuncs(gallery,
+		gv.userIDRequired,
+		gv.titleRequired)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
 var _ GalleryDB = &galleryGorm{}
 
 type galleryGorm struct {
@@ -40,4 +64,15 @@ type galleryGorm struct {
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+type galleryValFunc func(*Gallery) error
+
+func runGalleryValFuncs(gallery *Gallery, fns ...galleryValFunc) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
 }
